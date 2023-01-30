8fd27ab666b7df5eef4a118c6b31101f06ea2f54
Ingrid <ahtziringrid@hotmail.com>
2023-01-30 10:38:33 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Editarcandidato from './componentes/editarcandidaturas';
 function App() {
   return (
     <Router>
-      
       <div class="container-fluid" className='columnas'>
         <div class="row flex-nowrap">
           <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
@@ -28,27 +27,29 @@ function App() {
                     <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Ciudadanos</span> </a>
                   <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                     <li class="w-100">
-                    <Link class="nav-link" to="agregarciudadano">Registrar ciudadano</Link>
+                      <Link class="nav-link" to="agregarciudadano">Registrar ciudadano</Link>
                     </li>
                     <li>
-                    <Link class="nav-link" to="listar">Lista Nominal</Link>
+                      <Link class="nav-link" to="listar">Lista Nominal</Link>
                     </li>
                   </ul>
                 </li>
                 <li>
                   <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                    <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                    <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Candidaturas</span></a>
                   <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
                     <li class="w-100">
-                      <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+                      <Link class="nav-link" to="Registrarcandidaturas">Registrar Candidaturas</Link>
                     </li>
                     <li>
-                      <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+                      <Link class="nav-link" to="listarcandidatura">Listar Candidaturas</Link>
                     </li>
                   </ul>
                 </li>
+                <li>
+                  <Link class="nav-link px-0 align-middle" to="boleta">Boleta</Link>
+                </li>
               </ul>
-
               <div class="dropdown pb-4">
                 <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                   <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle" />
